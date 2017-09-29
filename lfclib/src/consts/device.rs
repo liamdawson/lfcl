5058453a24ecac05e4defd2583b91cfaec09a11d
Liam Dawson <dawmail333@gmail.com>
2017-09-29 13:33:39 +1000

    rainbow colours via argument (#1)
    * add clippy
    
    * remove clippy (not compatible with nightly .22)
    
    * add a lib for llcl
    
    * start lib
    
    * add constants
    
    * start filtering devices
    
    * working colour setting
    
    * support rainbow colours

---
+++ lfclib/src/consts/device.rs
@@ -0,0 +1,6 @@
+use device::LuxaforDeviceDescriptor;
+
+pub const FULL_FLAG : LuxaforDeviceDescriptor = LuxaforDeviceDescriptor {
+    vendor_id: 0x04d8,
+    product_id: 0xf372
+};
---
+++ lfclib/src/consts/mod.rs
@@ -0,0 +1,4 @@
+pub mod device;
+pub mod led;
+pub mod mode;
+pub mod pattern;
